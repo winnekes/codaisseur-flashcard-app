cefa9bfc6c052dde56c86312c611c8f13ce44cad
Simona Winnekes <simona.winnekes@googlemail.com>
2019-10-10 09:44:44 +0200

    updated css and visual of website

--- script.js
+++ script.js
@@ -11,42 +11,47 @@ const questions = [
     {
         question: 'What is the use of <em>isNaN function</em>?',
         answer: 'isNan function returns true if the argument is not a number otherwise it is false',
-        category: categories[0]
+        category: [categories[0]]
+    },
+    {
+        question: 'Explain what is pop()method in JavaScript?',
+        answer: 'The pop() method is similar as the shift() method but the difference is that the Shift method works at the start of the array. Also the pop() method take the last element off of the given array and returns it. The array on which is called is then altered.',
+        category: [categories[0]]
     },
     {
         question: 'What is "this" keyword in JavaScript?',
         answer: '"this" keyword refers to the object from where it was called',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'Operators: What is the <em>===</em> operator?',
         answer: '=== is called as strict equality operator which returns true when the two operands are having the same value without any type conversion',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'What are all the <em>looping</em> structures in JavaScript?',
         answer: 'Following are looping structures in Javascript: for while, do-while',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'Explain the difference between <em>"=="</em> and <em>"==="</em>?',
         answer: '"==" checks only for equality in value whereas "===" is a stricter equality test and returns false if either the value or the type of the two variables are different',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'What do we mean by <em>NULL</em> in Javascript?',
         answer: 'The NULL value is used to represent no value or no object. It implies no object or null string, no valid boolean value, no number and no array object',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'What are all the types of Pop up boxes available in JavaScript?',
         answer: 'Alert(), Confirm() and Prompt()',
-        category: categories[0]
+        category: [categories[0]]
     },
     {
         question: 'What does CSS stand for?',
         answer: 'Cascading Style Sheets',
-        category: categories[2]
+        category: [categories[2]]
     }
 ];
 function displayCategories() {
@@ -80,7 +85,7 @@ function toggleCard() {
 }
 
 function filterByCategory() {
-    
+
 }
 
 displayCategories();
