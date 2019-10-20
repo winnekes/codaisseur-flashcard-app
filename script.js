5784cdb206480b534389010981f798c7bc5cba80
Simona Winnekes <simona.winnekes@googlemail.com>
2019-10-20 21:17:36 +0200

    improved semantics

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const resetFlashCardsButton = document.getElementById('reset-flashcards');
 
 const categoriesOptions = document.getElementById('categories-options');
 
-
 // setting the local storage
 userStorage = window.localStorage;
 
@@ -21,18 +20,21 @@ userStorage = window.localStorage;
 
 let categories = ['JS', 'HTML', 'CSS'];
 
-let questions = JSON.parse(userStorage.getItem('questions')) || "";
+let questions = JSON.parse(userStorage.getItem('questions')) || '';
 
-let questionsData = [{
+let questionsData = [
+    {
         id: 1,
         question: 'What is the use of <em>isNaN function</em>?',
-        answer: 'isNan function returns true if the argument is not a number otherwise it is false',
+        answer:
+            'isNan function returns true if the argument is not a number otherwise it is false',
         category: 'JS'
     },
     {
         id: 2,
         question: 'Explain what is pop()method in JavaScript?',
-        answer: 'The pop() method is similar as the shift() method but the difference is that the Shift method works at the start of the array. Also the pop() method take the last element off of the given array and returns it. The array on which is called is then altered.',
+        answer:
+            'The pop() method is similar as the shift() method but the difference is that the Shift method works at the start of the array. Also the pop() method take the last element off of the given array and returns it. The array on which is called is then altered.',
         category: 'JS'
     },
     {
@@ -44,30 +46,36 @@ let questionsData = [{
     {
         id: 4,
         question: 'Operators: What is the <em>===</em> operator?',
-        answer: '=== is called as strict equality operator which returns true when the two operands are having the same value without any type conversion',
+        answer:
+            '=== is called as strict equality operator which returns true when the two operands are having the same value without any type conversion',
         category: 'JS'
     },
     {
         id: 5,
         question: 'What are all the <em>looping</em> structures in JavaScript?',
-        answer: 'Following are looping structures in Javascript: for while, do-while',
+        answer:
+            'Following are looping structures in Javascript: for while, do-while',
         category: 'JS'
     },
     {
         id: 6,
-        question: 'Explain the difference between <em>"=="</em> and <em>"==="</em>?',
-        answer: '"==" checks only for equality in value whereas "===" is a stricter equality test and returns false if either the value or the type of the two variables are different',
+        question:
+            'Explain the difference between <em>"=="</em> and <em>"==="</em>?',
+        answer:
+            '"==" checks only for equality in value whereas "===" is a stricter equality test and returns false if either the value or the type of the two variables are different',
         category: 'JS'
     },
     {
         id: 7,
         question: 'What do we mean by <em>NULL</em> in Javascript?',
-        answer: 'The NULL value is used to represent no value or no object. It implies no object or null string, no valid boolean value, no number and no array object',
+        answer:
+            'The NULL value is used to represent no value or no object. It implies no object or null string, no valid boolean value, no number and no array object',
         category: 'JS'
     },
     {
         id: 8,
-        question: 'What are all the types of Pop up boxes available in JavaScript?',
+        question:
+            'What are all the types of Pop up boxes available in JavaScript?',
         answer: 'Alert(), Confirm() and Prompt()',
         category: 'JS'
     },
@@ -80,13 +88,15 @@ let questionsData = [{
     {
         id: 10,
         question: 'What is HTML?',
-        answer: 'HTML stands for Hyper Text Markup Language. It is a language of World Wide Web. It is a standard text formatting language which is used to create and display pages on the Web. It makes the text more interactive and dynamic. It can turn text into images, tables, links.',
+        answer:
+            'HTML stands for Hyper Text Markup Language. It is a language of World Wide Web. It is a standard text formatting language which is used to create and display pages on the Web. It makes the text more interactive and dynamic. It can turn text into images, tables, links.',
         category: 'HTML'
     },
     {
         id: 11,
         question: 'What is the use of Canvas element?',
-        answer: 'The canvas element helps to build charts, graphs, bypass Photoshop to create 2D images and place them directly into HTML5 code',
+        answer:
+            'The canvas element helps to build charts, graphs, bypass Photoshop to create 2D images and place them directly into HTML5 code',
         category: 'HTML'
     }
 ];
@@ -95,12 +105,12 @@ if (userStorage.getItem('questions') !== null) {
     questions = JSON.parse(userStorage.getItem('questions'));
     onloadFunctions();
 } else {
-    /*     questions = questionsData;
-        userStorage.setItem('questions', JSON.stringify(questions)); */
+    /*
+     * userStorage.setItem('questions', JSON.stringify(questions)); */
 
     fetch('https://api.jsonbin.io/b/5d9f38a09fb3a828d4a68667')
         .then(response => {
-            return response.json()
+            return response.json();
         })
         .then(data => {
             userStorage.setItem('questions', JSON.stringify(data));
@@ -109,44 +119,48 @@ if (userStorage.getItem('questions') !== null) {
             onloadFunctions();
         })
         .catch(err => {
-            alert("Cannot use the Flashcard API - falling back to local data");
+            alert('Cannot use the Flashcard API - falling back to local data');
             console.log(err);
 
             questions = questionsData;
             onloadFunctions();
-        })
+        });
 }
 
 function onloadFunctions() {
     displayCategories();
-    displayRandomQuestion();
+    displayRandomFlashcard();
 
-    cardElement.addEventListener('click', toggleCard);
-    addFlashcardButton.addEventListener('click', toggleUserQuestionForm);
-    newFlashcardButton.addEventListener('click', () => displayRandomQuestion(''));
+    cardElement.addEventListener('click', toggleFlashcard);
+    addFlashcardButton.addEventListener('click', toggleUserFlashcardForm);
+    newFlashcardButton.addEventListener('click', () =>
+        displayRandomFlashcard('')
+    );
     resetFlashCardsButton.addEventListener('click', clearLocalUserStorage);
 }
 
-
 // empty localStorage
 function clearLocalUserStorage() {
-    confirmedClear = confirm("Are you sure you want to delete all the flashcards you added?");
+    confirmedClear = confirm(
+        'Are you sure you want to delete all the flashcards you added?'
+    );
     if (confirmedClear) {
         userStorage.clear();
-        jumpToAnchor('top')
-        alert('And we\'re back to the standard flashcards!');
+        jumpToAnchor('top');
+        alert("And we're back to the standard flashcards!");
     }
 }
 
-
-// display categories dynamically 
+// display categories dynamically
 function displayCategories() {
-
     for (let i = 0; i < categories.length; i++) {
         const categoryButton = document.createElement('button');
         categoryButton.setAttribute('id', 'cat-' + categories[i]);
-        categoryButton.setAttribute('onclick', 'displayRandomQuestion(\'' + categories[i] + '\');');
-        categoryButton.className = "category";
+        categoryButton.setAttribute(
+            'onclick',
+            "displayRandomFlashcard('" + categories[i] + "');"
+        );
+        categoryButton.className = 'category';
 
         categoryButton.innerHTML = categories[i];
         categoryListElement.appendChild(categoryButton);
@@ -158,14 +172,17 @@ function displayCategories() {
     }
 }
 
-// display a random card 
-function displayRandomFlashcard(category=false) {
+// display a random card
+function displayRandomFlashcard(category = false) {
     // always show the question side of the card first
     cardElement.classList.remove('is-flipped');
 
     if (category) {
         let filteredQuestions = [];
-        newFlashcardButton.setAttribute('onclick', 'displayRandomQuestion(\'' + category + '\')');
+        newFlashcardButton.setAttribute(
+            'onclick',
+            "displayRandomFlashcard('" + category + "')"
+        );
 
         for (let i = 0; i < questions.length; i++) {
             if (questions[i].category === category) {
@@ -174,20 +191,28 @@ function displayRandomFlashcard(category=false) {
         }
 
         if (filteredQuestions === undefined || filteredQuestions.length == 0) {
-            alert('There are no flashcards in this category. Why don\'t you add some?');
+            alert(
+                "There are no flashcards in this category. Why don't you add some?"
+            );
         } else {
             let randNum = Math.floor(Math.random() * filteredQuestions.length);
             questionElement.innerHTML = filteredQuestions[randNum].question;
             answerElement.innerHTML = filteredQuestions[randNum].answer;
-            deleteFlashcardButton.setAttribute('onclick', 'deleteFlashcard(\'' + filteredQuestions[randNum].id + '\')');
+            deleteFlashcardButton.setAttribute(
+                'onclick',
+                "deleteFlashcard('" + filteredQuestions[randNum].id + "')"
+            );
         }
     } else {
         let randNum = Math.floor(Math.random() * questions.length);
         questionElement.innerHTML = questions[randNum].question;
         answerElement.innerHTML = questions[randNum].answer;
 
-        deleteFlashcardButton.setAttribute('onclick', 'deleteFlashcard(\'' + questions[randNum].id + '\')');
-        newFlashcardButton.setAttribute('onclick', 'displayRandomQuestion()');
+        deleteFlashcardButton.setAttribute(
+            'onclick',
+            "deleteFlashcard('" + questions[randNum].id + "')"
+        );
+        newFlashcardButton.setAttribute('onclick', 'displayRandomFlashcard()');
     }
 
     userFlashcardFormElement.classList.remove('visible');
@@ -206,7 +231,7 @@ function addFlashcard(formData) {
         question: userQuestion,
         answer: userAnswer,
         category: userCategory
-    }
+    };
 
     questions.push(userFlashcard);
 
@@ -220,21 +245,19 @@ function addFlashcard(formData) {
 }
 
 function deleteFlashcard(flashcardID) {
-
-    const confirmedDelete = confirm("Are you sure you want to delete the current flashcard?");
+    const confirmedDelete = confirm(
+        'Are you sure you want to delete the current flashcard?'
+    );
     if (confirmedDelete) {
-
         for (let i = 0; i < questions.length; i++)
-
             if (questions[i].id == flashcardID) {
                 questions.splice(i, 1);
                 displayRandomFlashcard();
-                alert("The flashcard was deleted!");
+                alert('The flashcard was deleted!');
                 userStorage.setItem('questions', JSON.stringify(questions));
                 break;
             }
     }
-
 
     jumpToAnchor('top');
 }
